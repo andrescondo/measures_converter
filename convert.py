9459687b8d1179840fcedd10541b7f7708324a7b
Andres Condo <1dkevinandres.b@gmail.com>
2020-06-05 14:21:14 -0500

    added functions for mass calculation, and general function for receiving user data, and reduced number of decimals

--- convert.py
+++ convert.py
@@ -1,18 +1,44 @@
 # -*- coding: utf-8 -*-
 
+def value():
+	value = float(input('Ingrese el valor a convertir: '))
+	return value
+
 def kgAG():
 	print('Convertir de kg a G')
-	kg = float(input('Ingrese el valor a convertir: '))
+	kg = value()
 	g = kg * 1000
-	print(' {} Kilogramos, equivale a {} Gramos'.format(kg,g))
+	print(' {} Kilogramos, equivale a {:.3f} Gramos'.format(kg,g))
 
 def GaKg():
 	print('Convertir de Gramo a kg')
-	g = float(input('Ingrese el valor a convertir: '))
+	g = value()
 	kg = g / 1000
-	print(' {} Gramo, equivale a {} Kilogramos'.format(g,kg))
+	print(' {} Gramo, equivale a {:.3f} Kilogramos'.format(g,kg))
 
+def KgaLb():
+	print('Convertir de Kilogramos a libras')
+	kg = value()
+	lb = kg / 2.2                   #reducir el n# de decimales
+	print(' {} Kilogramos, equivale a {:.3f} libras'.format(kg, lb))
 
+def LbaKg():
+	print('Convertir de Libras a Kilogramos')
+	lb = value()
+	kg = lb * 0.453
+	print(' {} Libras, equivale a {:.3f} Kilogramos'.format(lb, kg))
+
+def GaOnz():
+	print('Convertir de Gramos a Onzas')
+	g = value()
+	onz = g * 0.035
+	print(' {} Gramos, equivale a {:.3f} Onzas'.format(g, onz))
+
+def OnaAG():
+	print('Convertir de Onzas a Gramos')
+	onz = value()
+	g = onz * 28.3495
+	print(' {} Onzas, equivale a {:.3f} Gramos'.format(onz. g))
 
 
 
@@ -25,15 +51,26 @@ def mass_convert():
 			Ingrese la medida a convertir
 			[1] Kg a G
 			[2] G a Kg
+			[3] Kg a Lb
+			[4] Lb a Kg
+			[5] G a Onz
+			[6] Onz a G
 
 			[s]alir
 			''')).lower()
 
 		if mass == '1':
 			kgAG()
-
 		elif mass == '2':
 			GaKg()
+		elif mass == '3':
+			KgaLb()
+		elif mass == '4':
+			LbaKg()
+		elif mass == '5':
+			GaOnz()
+		elif mass == '6':
+			OnaAG()
 
 		elif mass == 's':
 			break
@@ -44,6 +81,8 @@ def run():
 			Escoga el tipo de conversión
 			[m]asa
 			[v]olumen
+
+			[s]alir
 			''')).lower()
 
 		if option == 'm':
